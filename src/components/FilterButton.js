302fc099997bff084576e4691b6e9300faa3f210
Sam <swmoore84@gmail.com>
2020-11-26 17:16:15 +0900

    setup boilerplate with 3 components

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 function FilterButton(props) {
   return (
@@ -14,5 +15,14 @@ function FilterButton(props) {
     </button>
   );
 }
+
+FilterButton.propTypes = {
+   name: PropTypes.string,
+   isPressed: PropTypes.bool
+}
+
+FilterButton.defaultProps = {
+  name: 'Button Name'
+};
 
 export default FilterButton;
